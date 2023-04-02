7e091dc076a19cbef1f55b0cdb5d47d0403c5cf5
timeester3648 <weteringt@gmail.com>
2023-04-02 22:30:11 +0200

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -22,22 +22,3 @@ project "zstd"
  	filter "system:windows"
 		disablewarnings { "4244" }
 		defines { "_CRT_SECURE_NO_WARNINGS" }
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
